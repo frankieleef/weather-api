5ab4c483bf400d5121219314e2f52fe3e2c12603
Frank <frank.levering@projectivegroup.com>
2022-09-16 10:53:38 +0200

    Add tests

--- HelloWorld.Tsts/UnitTest1.cs
+++ HelloWorld.Tsts/UnitTest1.cs
@@ -1,5 +1,9 @@
+using HelloWorld.App;
 using HelloWorld.App.Services;
 using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
 
 namespace HelloWorld.Tsts;
 
@@ -11,5 +15,15 @@ public class UnitTest1
         var weatherService = new WeatherService();
         var weatherForecast = weatherService.GetForecast(DayOfWeek.Monday);
         Assert.Equal(1, (int)weatherForecast.Date.Day);
+    }
+
+    [Fact]
+    public async void GET_retrieve_weather_forecast()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        var response = await client.GetAsync("/weatherforecast");
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
